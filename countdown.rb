861d8ec46219076240318ceb47fb8b9751825b6f
voluble-bug-0219 <montensia1@outlook.com>
2019-05-13 01:45:03 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,10 @@
 #write your code here
 
-def countdown
+def countdown stopCounter
+  counter = 0
+  while counter < stopCounter
+    puts "#{counter} SECOND(S)!"
+    counter += 1
+  end
+    puts "HAPPY NEW YEAR!"
 end
