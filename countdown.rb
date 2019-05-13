313ac4530ee8330385d3e9642523e3ea653e93a9
voluble-bug-0219 <montensia1@outlook.com>
2019-05-13 01:56:13 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,20 @@
 #write your code here
 
-def countdown
+def countdown stopCounter
+  counter = stopCounter
+  while counter > 0
+    puts "#{counter} SECOND(S)!"
+    counter -= 1
+  end
+    return "HAPPY NEW YEAR!"
+end
+
+countdown_with_sleep
+  counter = stopCounter
+  while counter > 0
+    puts "#{counter} SECOND(S)!"
+    sleep(1)
+    counter -= 1
+  end
+    return "HAPPY NEW YEAR!"
 end
